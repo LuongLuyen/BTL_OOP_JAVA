4d4ac4818600c72140c1bd743181e1d15a476d20
LuongLuyen <luongvanluyen2003@gmail.com>
2023-08-22 09:14:11 +0700

    create_table database

---
+++ database/insert_table.sql
@@ -0,0 +1,5 @@
+Insert into users(userName,password,fullName,email,role) values('NguyenVanA',123,'Nguyễn Văn A','nguyenvana2003@gmail.com','ADMIN');
+Insert into users(userName,password,fullName,email,role) values('NguyenVanB',123,'Nguyễn Văn B','nguyenvanb2003@gmail.com','USER');
+
+Insert into product(userId,thumbnai,shortDescription,status,category,price) values(1,'https://down-vn.img.susercontent.com/file/sg-11134211-7qvd2-lffbx7p9vsyv95_tn','Áo Sơ Mi Nam Dài Tay Cổ Bẻ Form Rộng Thời Trang Hàn','stocking','ao-nam','100.000vnd');
+Insert into product(userId,thumbnai,shortDescription,status,category,price) values(1,'https://down-vn.img.susercontent.com/file/a0dd96347dabe97e3818f4fe8ea952a5_tn','Tai nghe gaming tai nghe chụp tai có mic có đèn LED cho máy tính, điện thoại','stocking','tai-nghe','200.000vnd');
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,13 +1,13 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
-<title>BTL</title>
+<meta charset="UTF-8">
+<title>BTL OOP JAVA</title>
 </head>
 <body>
-<h1> JAVA WEBSITE</h1>
+<h1>HELLO Đình Hưng</h1>
 
 </body>
 </html>
