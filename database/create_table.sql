90613abded5fa867aab8fe174272a782e2f0b2e7
LuongLuyen <luongvanluyen2003@gmail.com>
2023-09-26 20:57:49 +0700

    update database

--- database/create_table.sql
+++ database/create_table.sql
@@ -1,23 +1,3 @@
-CREATE DATABASE btloopjava ENCODING 'UTF8';
-
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  userName VARCHAR(150) NOT NULL,
-  password VARCHAR(150) NOT NULL,
-  fullName VARCHAR(127) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS product (
-  id SERIAL PRIMARY KEY NOT NULL,
-  userId INT NOT NULL,
-  shortDescription VARCHAR(255) NOT NULL,
-  status VARCHAR(225) NOT NULL,
-  category VARCHAR(150) NOT NULL,
-  price DOUBLE NOT NULL,
-  FOREIGN KEY (userId) REFERENCES users (id)
-);
-
-//mysql
 CREATE DATABASE btloopjava DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 use btloopjava;
 
@@ -32,6 +12,7 @@ CREATE TABLE IF NOT EXISTS product (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   userId INT NOT NULL,
   shortDescription VARCHAR(255) NOT NULL,
+  thumbnail VARCHAR(255) NOT NULL,
   status VARCHAR(225) NOT NULL,
   category VARCHAR(150) NOT NULL,
   price DOUBLE NOT NULL,
--- database/insert_table.sql
+++ database/insert_table.sql
@@ -1,11 +1,12 @@
 Insert into users(userName,password,fullName) values('NguyenVanA',123,'Nguyễn Văn A');
 Insert into users(userName,password,fullName) values('NguyenVanB',123,'Nguyễn Văn B');
 
-Insert into product(userId,shortDescription,status,category,price) values(1,'Áo Sơ Mi Nam Dài Tay Cổ Bẻ Form Rộng Thời Trang Hàn','100','ao-nam',100000);
-Insert into product(userId,shortDescription,status,category,price) values(1,'Smart Tivi Samsung Crystal 4K 65 inch UA65TU6900KXXV - Model 2020 - Miễn phí lắp đặt ','200','ti-vi',3200000);
-Insert into product(userId,shortDescription,status,category,price) values(1,'Điện Thoại Di Động Xiaomi Redmi 10 5G 4GB/64GB - Chính hãng','190','dien-thoai',4100000);
-Insert into product(userId,shortDescription,status,category,price) values(1,'Apple MacBook Air 13" 2020 (M1/8GB/256GB)','200','lap-top',19200000);
-Insert into product(userId,shortDescription,status,category,price) values(1,'Tai nghe gaming tai nghe chụp tai có mic có đèn LED cho máy tính, điện thoại','200','tai-nghe',500000);
+Insert into product(userId,shortDescription,thumbnail,status,category,price) values(1,'Áo Sơ Mi Nam Dài Tay Cổ Bẻ Form Rộng Thời Trang Hàn','https://down-vn.img.susercontent.com/file/sg-11134211-7qvd2-lffbx7p9vsyv95','100','ao-so-mi',299000);
+Insert into product(userId,shortDescription,thumbnail,status,category,price) values(1,'Áo thun nam cổ tròn vải cotton mát mẫu áo phông nam tay ngắn form rộng được mua nhiều nhất','https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-llkmqz5y6n9rb8','200','ao-thun',300000);
+Insert into product(userId,shortDescription,thumbnail,status,category,price) values(1,'Áo polo nam ngắn tay trơn thương hiệu 360 Boutique trắng đen chất liệu cao cấp - POTOL415','https://down-vn.img.susercontent.com/file/20806c891297463bc0f1bb025782e623','190','ao-polo',199000);
+Insert into product(userId,shortDescription,thumbnail,status,category,price) values(1,'Áo khoác ngoài, áo gió chống nước chống gió 2 lớp mỏng form rộng phong cách Hàn Quốc','https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-llawubdih3nse8','200','ao-khoac',599000);
+Insert into product(userId,shortDescription,thumbnail,status,category,price) values(1,'Quần short đùi trơn unisex ZUTEE thể thao basic nam nữ phong cách đường phố Hàn Quốc ullzang','https://down-vn.img.susercontent.com/file/c3ea98e7aae1b7a4f9be91983a5ab888','200','quan-dui',200000);
+Insert into product(userId,shortDescription,thumbnail,status,category,price) values(1,'Quần dài thể thao dáng rộng phong cách Hồng Kông thời trang thu đông 2023 dành cho nam','https://down-vn.img.susercontent.com/file/9d540a27a8b6ee769b61df57e99c33f1','200','quan-dai',350000);
 
 Insert into payment(userId,shortDescription,thumbnail,transport,category,price) values(1,'Áo Sơ Mi Nam Dài Tay Cổ Bẻ Form Rộng Thời Trang Hàn','https://down-vn.img.susercontent.com/file/sg-11134211-7qvd2-lffbx7p9vsyv95','cho-thanh-toan','ao-nam',100000);
 Insert into payment(userId,shortDescription,thumbnail,transport,category,price) values(1,'Áo Sơ Mi Nam Dài Tay Cổ Bẻ Form Rộng Thời Trang Hàn','https://down-vn.img.susercontent.com/file/sg-11134211-7qvd2-lffbx7p9vsyv95','cho-thanh-toan','ao-nam',100000);
